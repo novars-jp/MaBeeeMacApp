baa591e03416427b8783c8929e26e2090a3290c7
tksshj <tksshj@gmail.com>
2016-08-05 10:13:42 +0900

    mabeee.pyでURLも出力する

--- mabeee.py
+++ mabeee.py
@@ -4,14 +4,16 @@ import sys
 import requests
 import json
 
-
 baseurl = 'http://localhost:11111'
 
 path = '/'
 if (2 == len(sys.argv)):
     path = sys.argv[1]
 
-response = requests.get(baseurl + path)
+url = baseurl + path
+print url
+
+response = requests.get(url)
 
 print response.status_code
 if (0 < len(response.content)):
